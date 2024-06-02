d3f20f6f17dd646b8370d9b09b45675d6fdc3a20
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2024-06-02 10:47:08 +0200

    refactor and test stack and queue implementations

--- data_structures/stackAndQueue.js
+++ data_structures/stackAndQueue.js
@@ -1,76 +1,140 @@
-// example of stack implementation 
+const test = (title, callback) => {
+  console.log(` 🔹 ${title} 🔹 `);
+  callback();
+};
 
-class TextEditor {
-    constructor(){
-     this.text = [];
-     this.undone = [];
+/*
+ Example of stack implementation:
+*/
+(() => {
+  class TextEditor {
+    constructor() {
+      this.text = [];
+      this.undone = [];
     }
-    write(text){
-      this.text.push(text)
-      console.log(this.text.join(" "))
+    write(text) {
+      this.text.push(text);
     }
-    undo(){
-     this.undone.push(this.text[this.text.length-1]);   
-     this.text.pop();
-     console.log(this.text.join(" "));
+    undo() {
+      this.undone.push(this.text[this.text.length - 1]);
+      this.text.pop();
     }
-    redo(){
-     this.text.push(this.undone[this.undone.length-1]);
-     this.undone.pop()
-     console.log(this.text.join(" "));
+    redo() {
+      this.text.push(this.undone[this.undone.length - 1]);
+      this.undone.pop();
     }
+  }
 
-}
-
-// let textEditor = new TextEditor();
-// textEditor.write("hello poozh")
-// textEditor.write("i hope you're well");
-// textEditor.write("kisses");
-// textEditor.undo()
-// textEditor.redo();
-// textEditor.undo();
-// textEditor.write("i kiss u");
-
-// example of queue implementation 
-
-class Printer {
-    constructor(){
-        this.queue = [];
+  test("Text editor writes", () => {
+    const textEditor = new TextEditor();
+    textEditor.write("hello poozh");
+    if (textEditor.text.join(" ") === "hello poozh") {
+      console.log("✅ As expected");
+    } else {
+      console.log("❌ Not as expected");
     }
-    addToQueue(document){
-     this.queue.push(document)
-     console.log(`queued for printing: ${this.queue.join(';')}`)
+  });
+
+  test("Text editor undos", () => {
+    const textEditor = new TextEditor();
+    textEditor.write("hello poozh");
+    textEditor.write("hi");
+    textEditor.undo();
+    if (
+      textEditor.text.join(" ") === "hello poozh" &&
+      textEditor.undone.join(" ") === "hi"
+    ) {
+      console.log("✅ As expected");
+    } else {
+      console.log("❌ Not as expected");
     }
-    removeFromQueue(document){
-        let len = this.queue.length;
-        let updatedQueue = [];
-     for(let i=0; i<len; ++i){
-        if(this.queue[i] !== document){
+  });
+
+  test("Text editor redos", () => {
+    const textEditor = new TextEditor();
+    textEditor.write("hello poozh");
+    textEditor.write("hi");
+    textEditor.undo();
+    textEditor.redo();
+    if (
+      textEditor.text.join(" ") === "hello poozh hi" &&
+      textEditor.undone.join(" ") === ""
+    ) {
+      console.log("✅ As expected");
+    } else {
+      console.log("❌ Not as expected");
+    }
+  });
+})();
+
+/*
+ Example of queue implementation:
+*/
+(() => {
+  class Printer {
+    constructor() {
+      this.queue = [];
+    }
+    addToQueue(document) {
+      this.queue.push(document);
+    }
+    removeFromQueue(document) {
+      let len = this.queue.length;
+      let updatedQueue = [];
+      for (let i = 0; i < len; ++i) {
+        if (this.queue[i] !== document) {
           updatedQueue.push(this.queue[i]);
-        } else {
-            console.log(`${document} removed from queue`)
         }
-     }
-     if(updatedQueue.length === len){
-        console.log(`${document} not found`)
-     } else {
-       this.queue = updatedQueue; 
-     }
-     
-     console.log(`queued for printing: ${this.queue.join(";")}`);
+      }
+      if (updatedQueue.length === len) {
+        console.log(`${document} not found`);
+      } else {
+        this.queue = updatedQueue;
+      }
     }
-    print(){
-       let len = this.queue.length;
-       for(let i=0; i<len; ++i){
-        console.log(`finished printing: ${this.queue[i]}`)
-       }
+    getQueue() {
+      return this.queue.join(";");
     }
-}
+    print() {
+      let len = this.queue.length;
+      for (let i = 0; i < len; ++i) {
+        this.queue.shift();
+      }
+    }
+  }
 
-let hp333 = new Printer();
-hp333.addToQueue("birth certificate")
-hp333.addToQueue("university diploma");
-hp333.addToQueue("highschool diploma");
-hp333.addToQueue("marriage certificate");
-hp333.removeFromQueue("highschool diploma")
-hp333.print()
+  test("Printer adds to queue", () => {
+    const hp3 = new Printer();
+    hp3.addToQueue("birth certificate");
+    hp3.addToQueue("marriage certificate");
+    if (hp3.getQueue() === "birth certificate;marriage certificate") {
+      console.log("✅ As expected");
+    } else {
+      console.log("❌ Not as expected");
+    }
+  });
+
+  test("Printer removes from queue", () => {
+    const hp3 = new Printer();
+    hp3.addToQueue("birth certificate");
+    hp3.addToQueue("marriage certificate");
+    hp3.removeFromQueue("marriage certificate");
+    if (hp3.getQueue() === "birth certificate") {
+      console.log("✅ As expected");
+    } else {
+      console.log("❌ Not as expected");
+    }
+  });
+
+  test("Printer prints", () => {
+    const hp3 = new Printer();
+    hp3.addToQueue("birth certificate");
+    hp3.addToQueue("marriage certificate");
+    hp3.print();
+    if (hp3.getQueue() === "") {
+      console.log("✅ As expected");
+    } else {
+      console.log("❌ Not as expected");
+    }
+  });
+})();
