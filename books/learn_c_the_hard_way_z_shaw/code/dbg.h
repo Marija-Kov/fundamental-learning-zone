66e04f5016626024867c10fb57d97843d3bc6e12
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2024-07-17 11:49:36 +0200

    c the hard way z shaw: exercise 19, log func name

--- books/learn_c_the_hard_way_z_shaw/code/dbg.h
+++ books/learn_c_the_hard_way_z_shaw/code/dbg.h
@@ -11,21 +11,21 @@
 #define debug(M, ...) //..and we don't want to replace debug() with anything at compile time
 #else
 // print error, file and line where it occurred and any extra arguments; ... --> ##__VA_ARGS__ 
-#define debug(M, ...) fprintf(stderr, "DEBUG %s: %d: "M"\n",\
-		__FILE__, __LINE__, ##__VA_ARGS__) 
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: %s()): "M"\n",\
+		__FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno)) // safe, readable errno
 
 // The next 3 macros log messages meant for the end user
-#define log_err(M, ...) fprintf(stderr,	"[ERROR] (%s:%d: errno: %s) "M"\n",\
-		__FILE__, __LINE__,	clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr,	"[ERROR] (%s:%d: %s(): errno: %s) "M"\n",\
+		__FILE__, __LINE__,	__func__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) "M"\n",\
-		__FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: %s(): errno: %s) "M"\n",\
+		__FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) "M"\n",\
-		__FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d: %s()) "M"\n",\
+		__FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 // args: A - condition, M - error message
 #define check(A, M, ...) if(!(A)) {\
