3dd0e56d35ab87d81b1f3752dc45c5b21a29a0d8
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2024-09-11 22:30:11 +0200

    c the hard way z shaw: exercise 32

---
+++ books/learn_c_the_hard_way_z_shaw/code/liblcthw/dbg.h
@@ -0,0 +1,44 @@
+// Header guard ensures that the contents of the header file are only included once during compilation process
+#ifndef __dbg_h__ // checks of the macro has not been defined yet
+#define __dbg_h__ // defines the macro if above is true
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+// Conditionally compiled macro
+#ifdef NDEBUG // when NDEBUG macro is defined, we're in non-debug/release mode
+#define debug(M, ...) //..and we don't want to replace debug() with anything at compile time
+#else
+// print error, file and line where it occurred and any extra arguments; ... --> ##__VA_ARGS__ 
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: %s()): "M"\n",\
+		__FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#endif
+
+#define clean_errno() (errno == 0 ? "None" : strerror(errno)) // safe, readable errno
+
+// The next 3 macros log messages meant for the end user
+#define log_err(M, ...) fprintf(stderr,	"[ERROR] (%s:%d: %s(): errno: %s) "M"\n",\
+		__FILE__, __LINE__,	__func__, clean_errno(), ##__VA_ARGS__)
+
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: %s(): errno: %s) "M"\n",\
+		__FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
+
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d: %s()) "M"\n",\
+		__FILE__, __LINE__, __func__, ##__VA_ARGS__)
+
+// args: A - condition, M - error message
+#define check(A, M, ...) if(!(A)) {\
+	log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+
+// place inside any part of function that shouldn't run
+#define sentinel(M, ...) { log_err(M, ##__VA_ARGS__);	errno=0; goto error; }
+
+// makes sure the pointer is valid, otherwise report an error
+#define check_mem(A) check((A), "Out of memory.")
+
+// doesn't bother reporting if the error is common
+#define check_debug(A, M, ...) if(!(A)) {\
+	debug(M, ##__VA_ARGS__);	errno=0; goto error; }
+
+#endif
---
+++ books/learn_c_the_hard_way_z_shaw/code/liblcthw/src/lcthw/list.h
@@ -0,0 +1,40 @@
+#ifndef lcthw_List_h
+#define lcthw_List_h
+
+#include <stdlib.h>
+
+struct ListNode;
+typedef struct ListNode {
+  struct ListNode *next;
+  struct ListNode *prev;
+  void *value;
+} ListNode;
+
+typedef struct List {
+  int count;
+  ListNode *first;
+  ListNode *last;
+} List;
+
+List *List_create();
+void List_destroy(List *list);
+void List_clear(List *list);
+void List_clear_destroy(List *list);
+
+#define List_count(A)((A)->count)
+#define List_first(A)((A)->first != NULL ? (A)->first->value: NULL)
+#define List_last(A)((A)->last != NULL ? (A)->last->value: NULL)
+
+void List_push(List *list, void *value);
+void *List_pop(List *list);
+void List_unshift(List *list, void *value);
+void *List_shift(List *list);
+void *List_remove(List *list, ListNode *node);
+
+// L - list, F - first, N - next, C - current node
+#define LIST_FOREACH(L, F, N, C)\
+          ListNode *_node = NULL;\
+          ListNode *C = NULL;\
+          for(C = _node = L->F; _node != NULL; C = _node = _node->N)
+
+#endif
